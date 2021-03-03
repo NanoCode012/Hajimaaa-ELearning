b02cbcadfa2d0996210b9767604e5ed86237a595
Passara <passaraink@gmail.com>
2021-03-03 22:07:28 +0700

    changed title and added favion

--- landing.php
+++ landing.php
@@ -5,7 +5,7 @@
 		<meta name="author" content="www.frebsite.nl" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-        <title>LearnUp - Online Course & Education HTML Template</title>
+        <title>!Hajimaaa Elearning</title>
 
         <!-- Custom CSS -->
         <link href="assets/css/styles.css" rel="stylesheet">
