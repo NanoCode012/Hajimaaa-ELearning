657312c0cad2d94c03878fc98b32e5edeb9e18fe
Passara <passaraink@gmail.com>
2021-03-04 02:28:56 +0700

    Update login.php

--- login.php
+++ login.php
@@ -4,118 +4,36 @@
 		<meta charset="utf-8" />
 		<meta name="author" content="www.frebsite.nl" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <title>LearnUp - Online Course & Education HTML Template</title>
+        <title>!Hajimaaa Elearning System</title>
         <!-- Custom CSS -->
         <link href="assets/css/styles.css" rel="stylesheet">
 		<!-- Custom Color Option -->
 		<link href="assets/css/colors.css" rel="stylesheet">
     </head>
     <body class="red-skin gray">
-        <!-- ============================================================== -->
         <!-- Preloader - style you can find in spinners.css -->
-        <!-- ============================================================== -->
         <div id="preloader"><div class="preloader"><span></span><span></span></div></div>
-        <!-- ============================================================== -->
         <!-- Main wrapper - style you can find in pages.scss -->
-        <!-- ============================================================== -->
         <div id="main-wrapper">
-            <!-- ============================================================== -->
             <!-- Top header  -->
-            <!-- ============================================================== -->
             <!-- Start Navigation -->
 			<div class="header header-light head-shadow">
 				<div class="container">
 					<nav id="navigation" class="navigation navigation-landscape">
 						<div class="nav-header">
 							<a class="nav-brand" href="#">
-								<img src="assets/img/logo.png" class="logo" alt="" />
+								<img src="assets/img/hajima_full_logo_black.png" class="logo" alt="" />
 							</a>
 							<div class="nav-toggle"></div>
 						</div>
 						<div class="nav-menus-wrapper" style="transition-property: none;">
-							<ul class="nav-menu">
-								<li class="active"><a href="#">Home<span class="submenu-indicator"></span></a>
-									<ul class="nav-dropdown nav-submenu">
-										<li><a href="index.html">Home 1</a></li>
-										<li><a href="home-2.html">Home 2</a></li>
-										<li><a href="home-3.html">Home 3</a></li>
-										<li><a href="home-4.html">Home 4</a></li>
-										<li><a href="home-5.html">Home 5</a></li>
-										<li><a href="home-6.html">Home 6</a></li>
-										<li><a href="home-7.html">Home 7</a></li>
-										<li><a href="home-8.html">Home 8</a></li>
-										<li><a href="home-9.html">Home 9</a></li>
-										<li><a href="home-10.html">Home 10</a></li>
-									</ul>
+              <ul class="nav-menu nav-menu-social align-to-right">
+								<li class="login_click bg-red">
+									<a href="login.php">Sign in</a>
 								</li>
-								<li><a href="#">Courses<span class="submenu-indicator"></span></a>
-									<ul class="nav-dropdown nav-submenu">
-										<li><a href="#">Courses Grid Sidebar<span class="submenu-indicator"></span></a>
-											<ul class="nav-dropdown nav-submenu">
-												<li><a href="grid-with-sidebar.html">Courses grid 1</a></li>
-												<li><a href="grid-with-sidebar-2.html">Courses grid 1</a></li>
-												<li><a href="grid-with-sidebar-3.html">Courses grid 1</a></li>
-											</ul>
-										</li>
-										<li><a href="list-with-sidebar.html">List Layout with Sidebar</a></li>
-										<li><a href="#">Courses Grid Full Width<span class="submenu-indicator"></span></a>
-											<ul class="nav-dropdown nav-submenu">
-												<li><a href="full-width-course.html">Courses grid 1</a></li>
-												<li><a href="full-width-course-2.html">Courses grid 1</a></li>
-												<li><a href="full-width-course-3.html">Courses grid 1</a></li>
-												<li><a href="full-width-course-4.html">Courses grid 1</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Courses Detail<span class="submenu-indicator"></span></a>
-											<ul class="nav-dropdown nav-submenu">
-												<li><a href="detail.html">Course Detail 1</a></li>
-												<li><a href="detail-2.html">Course Detail 2</a></li>
-												<li><a href="detail-3.html">Course Detail 3</a></li>
-												<li><a href="detail-4.html">Course Detail 4</a></li>
-												<li><a href="detail-5.html">Course Detail 5</a></li>
-											</ul>
-										</li>
-										<li><a href="find-instructor.html">Find Instructor</a></li>
-										<li><a href="instructor-detail.html">Instructor Detail</a></li>
-									</ul>
+								<li class="login_click light">
+									<a href="register.php">Sign up</a>
 								</li>
-								<li><a href="#">Pages<span class="submenu-indicator"></span></a>
-									<ul class="nav-dropdown nav-submenu">
-										<li class=""><a href="#">User Dashboard<span class="submenu-indicator"></span></a>
-											<ul class="nav-dropdown nav-submenu">
-												<li><a href="dashboard.html">User Dashboard</a></li>
-												<li><a href="my-profile.html">My Profile</a></li>
-												<li><a href="all-courses.html">My Courses</a></li>
-												<li><a href="my-order.html">My Order</a></li>
-												<li><a href="saved-courses.html">Saved Courses</a></li>
-												<li><a href="reviews.html">My Reviews</a></li>
-												<li><a href="settings.html">My Settings</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Shop Pages<span class="submenu-indicator"></span></a>
-											<ul class="nav-dropdown nav-submenu">
-												<li><a href="shop-full-width.html">Shop Full Width</a></li>
-												<li><a href="shop-left-sidebar.html">Shop Sidebar Left</a></li>
-												<li><a href="shop-right-sidebar.html">Shop Sidebar Right</a></li>
-												<li><a href="product-detail.html">Shop Detail</a></li>
-												<li><a href="add-to-cart.html">Add To Cart</a></li>
-												<li><a href="product-wishlist.html">Wishlist</a></li>
-												<li><a href="checkout.html">Checkout</a></li>
-												<li><a href="shop-order.html">Order</a></li>
-											</ul>
-										</li>
-										<li><a href="about-us.html">About Us</a></li>
-										<li><a href="blog.html">Blog Style</a></li>
-										<li><a href="blog-detail.html">Blog Detail</a></li>
-										<li><a href="pricing.html">Pricing</a></li>
-										<li><a href="404.html">404 Page</a></li>
-										<li><a href="register.html">Register</a></li>
-										<li><a href="component.html">Elements</a></li>
-										<li><a href="privacy.html">Privacy Policy</a></li>
-										<li><a href="faq.html">FAQs</a></li>
-									</ul>
-								</li>
-								<li><a href="contact.html">Contact</a></li>
 							</ul>
 						</div>
 					</nav>
@@ -123,11 +41,7 @@
 			</div>
 			<!-- End Navigation -->
 			<div class="clearfix"></div>
-			<!-- ============================================================== -->
 			<!-- Top header  -->
-			<!-- ============================================================== -->
-
-			<!-- ========================== Login Section =============================== -->
 			<section>
 				<div class="container">
 					<div class="row justify-content-center">
@@ -135,6 +49,7 @@
 							<div class="log_wrap">
 								<h4>Sign In</h4>
 								<div class="login-form">
+                  <br><br><br>
 									<form>
 										<div class="form-group">
 											<label>Username</label>
@@ -163,12 +78,6 @@
 				</div>
 			</section>
 			<!-- ========================== End of Login Section =============================== -->
-			<!-- ============================ Footer Start ================================== -->
-			<footer class="dark-footer skin-dark-footer">
-			</footer>
-			<!-- ============================ Footer End ================================== -->
-			<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
-
 
 		</div>
 		<!-- End Wrapper -->
