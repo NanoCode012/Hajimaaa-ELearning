7bb80f68f3fa0432fff14687b45bbabedaeb9a80
Passara <passaraink@gmail.com>
2021-03-03 22:04:32 +0700

    added a landing page and log in page

---
+++ landing.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="author" content="www.frebsite.nl" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+        <title>LearnUp - Online Course & Education HTML Template</title>
+
+        <!-- Custom CSS -->
+        <link href="assets/css/styles.css" rel="stylesheet">
+
+		<!-- Custom Color Option -->
+		<link href="assets/css/colors.css" rel="stylesheet">
+    <!-- Favicon -->
+    <link rel="icon" href="assets/img/hajima_logo_black.png" type="image/png">
+
+    </head>
+
+    <body class="red-skin">
+        <!-- Preloader - style you can find in spinners.css -->
+        <div id="preloader"><div class="preloader"><span></span><span></span></div></div>
+        <!-- Main wrapper - style you can find in pages.scss -->
+        <div id="main-wrapper">
+            <!-- Top header  -->
+            <!-- Start Navigation -->
+			<div class="header header-transparent change-logo">
+				<div class="container">
+					<nav id="navigation" class="navigation navigation-landscape">
+						<div class="nav-header">
+							<a class="nav-brand static-logo" href="landing.php"><img src="assets/img/hajima_full_logo_white.png" class="logo" alt="" /></a>
+							<div class="nav-toggle"></div>
+						</div>
+						<div class="nav-menus-wrapper" style="transition-property: none;">
+							<ul class="nav-menu nav-menu-social align-to-right">
+								<li class="login_click light">
+									<a href="login.php">Sign in</a>
+								</li>
+								<li class="login_click bg-green">
+									<a href="register.php">Sign up</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+			<!-- End Navigation -->
+			<div class="clearfix"></div>
+			<!-- Top header  -->
+			<!-- ============================ Hero Banner  Start================================== -->
+			<div class="image-cover hero_banner hero-inner-2" style="background:#032656;" data-overlay="0">
+				<div class="container">
+					<!-- Type -->
+          <br><br><br><br>
+					<div class="row align-items-center">
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="banner-search-2 transparent">
+								<h1 class="big-header-capt cl_2 mb-2 f_2">!Hajimaaa<br>Elearning Platform</h1>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<div class="mt-4">
+									<a href="login.php" class="btn btn-theme btn-lg"> Join Us! </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-12">
+							<div class="flixio pt-5">
+                <img class="img-fluid" src="assets/img/youtubetutorial.png" alt="">
+              </div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<!-- ============================ Hero Banner End ================================== -->
+			<!-- ============================ Footer Start ================================== -->
+			<footer style="background:#032656;">
+			</footer>
+			<!-- ============================ Footer End ================================== -->
+			<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+		</div>
+		<!-- End Wrapper -->
+		<!-- All Jquery -->
+		<!-- ============================================================== -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/select2.min.js"></script>
+		<script src="assets/js/slick.js"></script>
+		<script src="assets/js/jquery.counterup.min.js"></script>
+		<script src="assets/js/counterup.min.js"></script>
+		<script src="assets/js/custom.js"></script>
+		<!-- ============================================================== -->
+	</body>
+</html>
