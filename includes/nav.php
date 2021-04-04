3e77cd459814cf89d8fd43c9817870e33419ffd7
Littel-Cactus <80808493+Littel-Cactus@users.noreply.github.com>
2021-04-04 15:07:32 +0700

    Header

---
+++ includes/nav.php
@@ -0,0 +1,36 @@
+        <!-- Start Navigation -->
+        <div class="header header-light head-shadow">
+            <div class="container">
+                <nav id="navigation" class="navigation navigation-landscape">
+                    <div class="nav-header">
+                        <a class="nav-brand" href="#">
+                            <img src="assets/img/hajima_full_logo_black.png" class="logo" alt="" />
+                        </a>
+                        <div class="nav-toggle"></div>
+                    </div>
+                    <div class="nav-menus-wrapper" style="transition-property: none;">
+                        <ul class="nav-menu">
+                        
+                            <li><a href="#">Home<span class="submenu-indicator"></span></a>
+                            </li>
+                            
+                            <li  class="active"><a href="#">Classes<span class="submenu-indicator"></span></a>
+                            </li>
+                            
+                            <li><a href="#">Timetable<span class="submenu-indicator"></span></a>
+                            </li>
+                            
+                            <li><a href="contact.html">Settings</a></li>
+                            
+                        </ul>
+                        
+                        <ul class="nav-menu nav-menu-social align-to-right">
+                            <li class="login_click theme-bg">
+                                <a href="register.html">Log Out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <!-- End Navigation -->
