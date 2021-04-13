a9079f84a28234e7b98f8ee897835baaea7f72e6
WK <80820713+WarisKM@users.noreply.github.com>
2021-04-13 09:18:56 +0700

    1.0

---
+++ pages/classlist.php
@@ -0,0 +1,268 @@
+<body class="red-skin">
+    <!-- Preloader - style you can find in spinners.css -->
+    <div id="preloader">
+        <div class="preloader"><span></span><span></span></div>
+    </div>
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <div id="main-wrapper">
+        <!-- Top header  -->
+        <?php include "includes/nav.php";?>
+        <div class="clearfix"></div>
+        <!-- Top header  -->
+        <div class="">
+                                <div class="col-lg-12 col-md-12 col-sm-12 pt-10 pb-10">
+									<nav aria-label="breadcrumb">
+										<ol class="breadcrumb">
+											<li class="breadcrumb-item"><a href="#">Home</a></li>
+                                            <li class="breadcrumb-item" aria-current="page"><a href="#">Assignment</a></li> 
+                                            <li class="breadcrumb-item" aria-current="page"><a href="#">Calendar</a></li>
+											<li class="breadcrumb-item active" aria-current="page"><a href="#">Classes</a></li>
+										</ol>
+    
+                                        <div class="col-lg-12 col-md-12 col-sm-12 pt-8 pb-8">
+                                        <ul class="greeting" allign-item= "center">
+                                            <H2><p> Welcome back Steph </H2></P>
+                                        </ul>
+
+                                        <div class="col-lg-12 col-md-12 col-sm-12 pt-8 pb-8">
+                                        <ul class="nav-menu nav-menu-social align-to-right">
+                                            <!-- <li class="buttonC"><a href="#">Create New Class    </a></li> !-->
+                                            <button type="submit" class="btn btn-theme"><a href ="NewClass">Create a New Class</button>
+                                            
+                                        </div>
+									</nav>
+								</div>
+							</div>
+        
+         
+
+
+						
+
+					
+        <section>
+            <div class="container">
+            <div class="row">
+                       
+
+
+                        <div class="col-lg-9 col-md-12 col-sm-12" >
+                            <div class="row">
+                                
+
+                                
+                                <div class="col-lg-6.5 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt="center" style="max-height: 400px; max-width: 350px;" ></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                                <h4><a href="#">PHP Basic & SQL</a></h4>
+                                                <span>2 Section</span>
+                                                <span> | Time: 13:45 - 16:45</span>
+
+                                                <br><small><p style = "color: white">Learn new languages and abilty to develop design</small></p>
+                                            </div>
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-6 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt=""></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                            <h4><a href="#">Technical Writing</a></h4>
+                                                <span>5 Section</span>
+                                                <span> | Time: 11:45 - 13:45</span>
+                                                <br><small><p style = "color: white">Learn how to write in more efiicient way</small></p>
+                                            </div>
+
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6.5 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt="center" style="max-height: 400px; max-width: 350px;" ></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                                <h4><a href="#">Computer Networking</a></h4>
+                                                <span>1 Section</span>
+                                                <span> | Time: 13:45 - 16:45</span>
+
+                                                <br><small><p style = "color: white">Learn Assembly language to understand network protocol</small></p>
+                                            </div>
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-lg-6.5 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt="center" style="max-height: 400px; max-width: 350px;" ></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                                <h4><a href="#">General Science II</a></h4>
+                                                <span>6 Section</span>
+                                                <span> | Time: 9:45 - 12:45</span>
+
+                                                <br><small><p style = "color: white">Learn physics principle and formulas</small></p>
+                                            </div>
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6.5 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt="center" style="max-height: 400px; max-width: 350px;" ></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                                <h4><a href="#">Computer graphics</a></h4>
+                                                <span>2 Section</span>
+                                                <span> | Time: 13:45 - 16:45</span>
+
+                                                <br><small><p style = "color: white">Mathlab and OpenGI</small></p>
+                                            </div>
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6.5 col-md-6 col-sm-12">
+                                    <div class="course_overlay_cat">
+                                        <div class="course_overlay_cat_thumb">
+                                            <a href="#" tabindex="0"><img src="https://miro.medium.com/max/3960/0*HICLyAdNSIyT0ODU.jpg" class="img-fluid" alt="center" style="max-height: 400px; max-width: 350px;" ></a>
+                                        </div>
+                                        <div class="course_overlay_cat_caption">
+                                            <div class="llp-left">
+                                                <h4><a href="#">Asean Study</a></h4>
+                                                <span>8 Section</span>
+                                                <span> | Time: 13:00 - 16:00</span>
+
+                                                <br><small><p style = "color: white">Learn about south east asian history</small></p>
+                                            </div>
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <<a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/235/235251.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <<a href="assignment1"><img src="https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_assignment_48px-512.png" style="max-height: 50px; max-width: 40px;"></a>>
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+            
+            
+            
+                            </div>
+                </div>
+                         <div class="col-lg-3 col-md-3 " >
+                            
+						
+
+                                <div class="ed_view_box style_2">
+                                
+                            
+                                    <div class="ed_view_features pl-4">
+                                        <span>Assignment</span>
+                                        <ul>
+                                            <li><i class="ti-angle-right"></i>Programming</li>
+                                            <li><i class="ti-angle-right"></i>Do exercise #42</li>
+                                            <li><i class="ti-angle-right"></i>Assembly Project</li>
+                                            <li><i class="ti-angle-right"></i>Progress Report</li>
+                                            <li><i class="ti-angle-right"></i>Research</li>
+                                        </ul>
+                                    </div>
+                                
+                               <!-- <div class="quiz">
+                                    <div class="row">
+                                        <p> Quizes </p>
+                                    </div>
+                                </div>
+                                    <!-->
+                            </div>	
+                                
+                               
+
+                            <div class="col-lg-3 col-md-3">
+
+                                            <div class="banner" >
+                                                    <div class="classfolder1">
+                                                        <a href="classfolder1"><img src="https://image.flaticon.com/icons/png/512/1430/1430896.png " style="max-height: 50px; max-width: 40px;"></a>
+                                                       
+                                                    </div>
+
+                                                    <div class="Assignment1">
+                                                        <a href="assignment1"><img src="https://image.flaticon.com/icons/png/512/1430/1430896.png" style="max-height: 50px; max-width: 40px;"></a>  
+                                                    </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
+                        
+                    </div>
+
+
+                           
+        </section>
+        <!-- ========================== End of Login Section =============================== -->
+    </div>
+    
+    <!-- End Wrapper -->
