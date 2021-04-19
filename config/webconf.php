5a2305d069445d6cb92e45283772dbc1e6f8408e
NanoCode012 <kevinvong@rocketmail.com>
2021-04-19 15:07:49 +0700

    Revert webconf

--- config/webconf.php
+++ config/webconf.php
@@ -18,8 +18,7 @@ else {
         'register',
         'landing',
         'accountmanager',
-        'create_account',
-        'editpfp'])
+        'create_account'])
     ) {
         $page = $_GET['p'];
     } else {
