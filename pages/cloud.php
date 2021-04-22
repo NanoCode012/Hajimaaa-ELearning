697d098664c3f52b573e37314b375505ef5f8be3
NanoCode012 <kevinvong@rocketmail.com>
2021-04-22 21:28:14 +0700

    Delete sample test file

--- pages/cloud.php
+++
@@ -1,7 +0,0 @@
-<body>
-
-    <?php include 'includes/utils/gcloud.php';
-    $gstorage = new GStorage();
-    $gstorage->download('file.txt', 'file.txt');
-    $gstorage->upload('file2.txt', 'file2.txt');
-    ?>
