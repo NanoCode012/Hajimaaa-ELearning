b509be2440a4e72399d7db64b53084323529a926
Littel-Cactus <80808493+Littel-Cactus@users.noreply.github.com>
2021-04-23 00:07:05 +0700

    Update Now page to use backend (#32)
    * Now-Page
    
    Now-Page for both Students and Teacher
    -Create Post
    -Remove view button
    
    * Updated to use GET variables and cleaned up
    
    Co-authored-by: NanoCode012 <kevinvong@rocketmail.com>

--- pages/Now-Student.php
+++ pages/Now-Student.php
@@ -1,5 +1,4 @@
 <body class="red-skin gray">
-
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
     <!-- ============================================================== -->
@@ -13,188 +12,164 @@
     <!-- Main wrapper - style you can find in pages.scss -->
     <!-- ============================================================== -->
     <div id="main-wrapper">
+
         <?php include 'includes/nav.php'; ?>
         <div class="clearfix"></div>
-        <div class="col-lg-12 col-md-9 col-sm-12">
-
-            <!-- Row -->
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 pt-4 pb-4">
-                    <nav aria-label="breadcrumb">
-                        <div class="breadcrumb">
-                            <div>
-                                <h1>CSS-323 Software Engineering</h1>
-                                <h4>Instructor: Asst. Prof. Dr. Sasiporn Usanavasin</h4>
-                                <h4>Contact: Email@email.com</h4>
-                            </div>
-                        </div>
-                    </nav>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <!-- tabs-->
-
-                            <div class="tabs">
-                                <div class="tab-header">
-                                    <div class="active">
-                                        <a href="?p=now-student">Now</a>
-                                    </div>
-                                    <div>
-                                        <a href="?p=ass-student">Assignments</a>
-                                    </div>
-                                    <div>
-                                        <a href="?p=lecturestudent">Lecture Notes</a>
-                                    </div>
-                                </div>
-                                <div class="tab-indicator"></div>
-                                <div class="tab-body">
-                                    <div class="active">
-                                    </div>
-                                    <div class="active">
-                                    </div>
-                                    <div class="active">
-                                    </div>
-                                </div>
-                            </div>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
-
-            <!-- /tabs-->
-            <!-- /Row -->
+        <!-- ============================================================== -->
+        <!-- Top header  -->
+        <!-- ============================================================== -->
 
 
-            <!-- Row -->
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
+        <!-- ============================ Dashboard: My Order Start ================================== -->
+        <section class="gray pt-0">
 
-                    <!-- Course Style 1 For Student -->
-                    <div class="dashboard_container">
-                        <div class="dashboard_container_body">
-
-                            <!-- Single Course -->
-
-                            <div class="dashboard_single_course">
-
-
-                                <div class="dashboard_single_course_caption">
-                                    <div class="dashboard_single_course_head">
-                                        <div class="dashboard_single_course_head_flex">
-                                            <h2 class="dashboard_course_title">Quiz on Thursday</h2>
-
-                                        </div>
-                                        <div class="dc_head_right">
-                                            <h4 class="dc_price_rate theme-cl"></h4>
-                                        </div>
-                                    </div>
-                                    <div class="dashboard_single_course_des">
-                                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                                            praesentium voluptatum deleniti atque corrupti quos dolores et quas
-                                            molestias excepturi sint occaecati cupiditate non provident, similique
-                                            sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-                                            dolorum fuga.</p>
-                                    </div>
-                                    <div class="dashboard_single_course_progress">
-                                        <div class="dashboard_single_course_progress_1">
-                                            <div class="ass-buttons">
-                                                <a href="?p=view-post" class="btn btn-theme-2">View</a>
-                                            </div>
-
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            <div class="container">
 
 
 
-                            <!-- Single Course -->
+                <!-- Row -->
+                <div class="row">
 
-                            <div class="dashboard_single_course">
 
 
-                                <div class="dashboard_single_course_caption">
-                                    <div class="dashboard_single_course_head">
-                                        <div class="dashboard_single_course_head_flex">
-                                            <h4 class="dashboard_course_title">No class next week due to Songkran
+
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                        <!--style="margin: auto; width: 50%;"-->
+
+                        <!-- Row -->
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 pt-4 pb-4">
+                                <div class="dashboard_container">
+                                    <?php
+                                    $class_id = $_GET['class_id'];
+                                    $stmt = $db_r->prepare('SELECT class_name,class_code,class_instructor FROM class WHERE class_id=?');
+                                    $stmt->execute([$class_id]);
+                                    $get = $stmt->fetch();
+                                    ?>
+                                    <div class="dashboard_container_header">
+                                        <div class="dashboard_fl_1">
+                                            <h1><?php echo $get['class_code']; ?> <?php echo $get['class_name']; ?></h1>
+                                            <h4 class="edu_title">Instructor: <?php echo $get['class_instructor'] ?>
                                             </h4>
+                                            <span class="dashboard_instructor">Contact: Email@email.com</span>
 
                                         </div>
-                                        <div class="dc_head_right">
-                                            <h4 class="dc_price_rate theme-cl"></h4>
-                                        </div>
-                                    </div>
-                                    <div class="dashboard_single_course_des">
-                                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                                            praesentium voluptatum deleniti atque corrupti quos dolores et quas
-                                            molestias excepturi sint occaecati cupiditate non provident, similique
-                                            sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-                                            dolorum fuga.</p>
-                                    </div>
-                                    <div class="dashboard_single_course_progress">
-                                        <div class="dashboard_single_course_progress_1">
-                                            <div class="ass-buttons">
-                                                <a href="?p=view-post" class="btn btn-theme-2">View</a>
-                                            </div>
 
-                                        </div>
+
                                     </div>
                                 </div>
-                            </div>
-
-                            <!-- Single Course -->
-
-                            <div class="dashboard_single_course">
 
 
-                                <div class="dashboard_single_course_caption">
-                                    <div class="dashboard_single_course_head">
-                                        <div class="dashboard_single_course_head_flex">
-                                            <h4 class="dashboard_course_title">Please submit your report</h4>
 
-                                        </div>
-                                        <div class="dc_head_right">
-                                            <h4 class="dc_price_rate theme-cl"></h4>
-                                        </div>
-                                    </div>
-                                    <div class="dashboard_single_course_des">
-                                        <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                                            praesentium voluptatum deleniti atque corrupti quos dolores et quas
-                                            molestias excepturi sint occaecati cupiditate non provident, similique
-                                            sunt in culpa qui officia deserunt mollitia animi, id est laborum et
-                                            dolorum fuga.</p>
-                                    </div>
-                                    <div class="dashboard_single_course_progress">
-                                        <div class="dashboard_single_course_progress_1">
-                                            <div class="ass-buttons">
-                                                <a href="?p=view-post" class="btn btn-theme-2">View</a>
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <!-- tabs-->
+
+                                        <div class="tabs">
+                                            <div class="tab-header">
+                                                <div class="active">
+                                                    <a href="?p=now-teacher">Now</a>
+                                                </div>
+                                                <div>
+                                                    <a href="?p=ass-teacher">Assignments</a>
+                                                </div>
+                                                <div>
+                                                    <a href="?p=lectureteacher">Lecture Notes</a>
+                                                </div>
+
                                             </div>
+                                            <div class="tab-indicator"></div>
 
+                                            <div class="tab-body">
+                                                <div class="active">
+
+
+                                                </div>
+                                                <div class="active">
+
+
+                                                </div>
+                                                <div class="active">
+
+
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
+                                    </ol>
+                                </nav>
                             </div>
-
-
-
-
-
-
                         </div>
+
+                        <!-- /tabs-->
+                        <!-- /Row -->
+
+
+                        <!-- Row -->
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+
+                                <!-- Course Style 1 For Student -->
+
+                                <div class="dashboard_container">
+                                    <div class="dashboard_container_body">
+
+                                        <!-- Single Course -->
+                                        <?php
+                                        //$class_id=$_GET['class_id'];
+                                        //$stmt = $db_r->query('SELECT * FROM posts WHERE class_id=$class_id ORDER BY time_created');
+                                        $stmt = $db_r->query('SELECT * FROM posts ORDER BY time_created');
+                                        while ($row = $stmt->fetch()) {
+                                        ?>
+
+                                        <a
+                                            href="?p=view-post&class_id=<?= $_GET['class_id'] ?>&post_id=<?php echo $row['post_id'] ?>">
+                                            <div style="cursor: pointer;"
+                                                class="dashboard_single_course ass_hover_effect">
+
+
+                                                <div class="dashboard_single_course_caption">
+                                                    <div class="dashboard_single_course_head">
+                                                        <div class="dashboard_single_course_head_flex">
+                                                            <span class="dashboard_instructor"></span>
+                                                            <h4 class="dashboard_course_title">
+                                                                <?php echo $row['title'] ?> </h4>
+
+                                                        </div>
+                                                        <div class="dc_head_right">
+                                                            <h4 class="dc_price_rate theme-cl"></h4>
+                                                        </div>
+                                                    </div>
+                                                    <div class="dashboard_single_course_des">
+                                                        <p> <?php echo $row['description'] ?> </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </a>
+                                        <?php } ?>
+
+
+
+
+
+                                    </div>
+                                </div>
+
+
+
+
+                            </div>
+                        </div>
+                        <!-- /Row -->
+
                     </div>
 
                 </div>
+                <!-- Row -->
+
             </div>
-            <!-- /Row -->
+        </section>
 
-        </div>
-
-    </div>
-    <!-- Row -->
-
-    </div>
-    </section>
-
-    <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+        <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
 
 
     </div>
@@ -202,11 +177,6 @@
     <!-- End Wrapper -->
     <!-- ============================================================== -->
 
-
-
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
 
     <!--JS for tabs-->
     <script>
