b80890f7e0cce38dd4bf3e821327ec3cb1a141ed
NanoCode012 <kevinvong@rocketmail.com>
2021-04-23 23:00:39 +0700

    Update folder.php mkdir permission (#36)
    * Update folder.php
    
    * Update .gitignore

--- config/folder.php
+++ config/folder.php
@@ -4,6 +4,6 @@ $folders_to_create = array('assets/files', 'assets/files/course_images');
 
 foreach ($folders_to_create as $folder) {
     if (!file_exists($folder)) {
-        mkdir($folder);
+        mkdir($folder, 0755);
     }
 }
