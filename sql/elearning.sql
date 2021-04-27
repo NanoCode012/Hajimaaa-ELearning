e537cb3e31b45897af5953e16c60c19b50d53271
Passara <passaraink@gmail.com>
2021-04-28 00:33:25 +0700

    new sql file

--- sql/elearning.sql
+++ sql/elearning.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Apr 26, 2021 at 06:26 PM
--- Server version: 5.7.24
--- PHP Version: 7.4.1
+-- Host: localhost
+-- Generation Time: Apr 27, 2021 at 07:32 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,18 +20,13 @@ SET time_zone = "+00:00";
 --
 -- Database: `elearning`
 --
-CREATE DATABASE IF NOT EXISTS `elearning` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `elearning`;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `assignments`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `assignments`;
 CREATE TABLE `assignments` (
   `assignment_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
@@ -41,63 +35,36 @@ CREATE TABLE `assignments` (
   `due_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONSHIPS FOR TABLE `assignments`:
---   `post_id`
---       `posts` -> `post_id`
---
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `class`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `class`;
 CREATE TABLE `class` (
   `class_id` int(11) NOT NULL,
   `class_name` varchar(255) NOT NULL,
   `class_code` varchar(10) DEFAULT NULL,
-  `class_description` mediumtext,
+  `class_description` mediumtext DEFAULT NULL,
   `categories` varchar(255) NOT NULL,
   `instructor_id` int(11) NOT NULL,
   `class_instructor` varchar(255) DEFAULT NULL,
   `class_secret` varchar(20) NOT NULL,
   `course_img_path` varchar(255) NOT NULL,
-  `time_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `class`:
---   `instructor_id`
---       `users` -> `user_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `class_enrolled`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
--- Last update: Apr 26, 2021 at 03:14 PM
---
 
-DROP TABLE IF EXISTS `class_enrolled`;
 CREATE TABLE `class_enrolled` (
   `enrollment_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `class_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `class_enrolled`:
---   `class_id`
---       `class` -> `class_id`
---   `user_id`
---       `users` -> `user_id`
---
 
 -- --------------------------------------------------------
 
@@ -105,7 +72,6 @@ CREATE TABLE `class_enrolled` (
 -- Stand-in structure for view `class_enrolled_count`
 -- (See below for the actual view)
 --
-DROP VIEW IF EXISTS `class_enrolled_count`;
 CREATE TABLE `class_enrolled_count` (
 `class_id` int(11)
 ,`num_students` bigint(21)
@@ -116,100 +82,58 @@ CREATE TABLE `class_enrolled_count` (
 --
 -- Table structure for table `comments`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `comments`;
 CREATE TABLE `comments` (
   `comment_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `comment` mediumtext NOT NULL,
-  `time_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `comments`:
---   `post_id`
---       `posts` -> `post_id`
---   `user_id`
---       `users` -> `user_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `files`
 --
--- Creation: Apr 25, 2021 at 08:26 AM
---
 
-DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `file_id` int(11) NOT NULL,
   `assignments_id` int(11) NOT NULL,
   `file_name` text NOT NULL,
   `file_path` mediumtext NOT NULL,
-  `time_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `files`:
---   `assignments_id`
---       `assignments` -> `assignment_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `gradeass`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
--- Last update: Apr 26, 2021 at 05:59 PM
---
 
-DROP TABLE IF EXISTS `gradeass`;
 CREATE TABLE `gradeass` (
   `student_file_id` int(11) NOT NULL,
   `marks` int(3) NOT NULL,
   `comments` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONSHIPS FOR TABLE `gradeass`:
---   `student_file_id`
---       `student_files` -> `student_file_id`
---
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `lectures`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `lectures`;
 CREATE TABLE `lectures` (
   `lecture_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `lectures`:
---   `post_id`
---       `posts` -> `post_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `posts`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL,
   `class_id` int(11) NOT NULL,
@@ -217,27 +141,15 @@ CREATE TABLE `posts` (
   `post_type` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
-  `time_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `posts`:
---   `class_id`
---       `class` -> `class_id`
---   `user_id`
---       `users` -> `user_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `student_files`
 --
--- Creation: Apr 25, 2021 at 09:13 AM
--- Last update: Apr 26, 2021 at 05:51 PM
---
 
-DROP TABLE IF EXISTS `student_files`;
 CREATE TABLE `student_files` (
   `student_file_id` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
@@ -245,26 +157,15 @@ CREATE TABLE `student_files` (
   `text_answer` text NOT NULL,
   `file_name` varchar(255) NOT NULL,
   `file_path` mediumtext NOT NULL,
-  `time_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `time_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- RELATIONSHIPS FOR TABLE `student_files`:
---   `student_id`
---       `users` -> `user_id`
---   `assignment_id`
---       `assignments` -> `assignment_id`
---
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
--- Creation: Apr 25, 2021 at 07:55 AM
---
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` int(255) NOT NULL,
   `user_type` int(255) NOT NULL,
@@ -283,8 +184,13 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONSHIPS FOR TABLE `users`:
+-- Dumping data for table `users`
 --
+
+INSERT INTO `users` (`user_id`, `user_type`, `username`, `firstname`, `lastname`, `password`, `email`, `title`, `phone`, `address`, `city`, `state`, `zip`, `about`) VALUES
+(1, 0, 'passaraink', 'Passara', 'Chanchotisatien', '$2y$10$Jf3JD/l5AQN9Pkemsjmn9ueaKUQOxQ5IpgiSKJDEYUcOh8g85HT2.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 0, 'kevinvong', 'Kevin', 'Vong', '$2y$10$kNQ5zG73/qHTMmnCB6aN4u2oWPC4zJUkyJ9X/NG29Wes.XZrArH5u', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 1, 'piyatou', 'Piya', 'Tou', '$2y$10$edudnczqtq8jvmrcsftjAOMPycbToM.fCZ3s7E1joR/oYUXpsM.Ai', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -293,7 +199,7 @@ CREATE TABLE `users` (
 --
 DROP TABLE IF EXISTS `class_enrolled_count`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `class_enrolled_count`  AS  select `c`.`class_id` AS `class_id`,count(`ce`.`user_id`) AS `num_students` from (`class` `c` left join `class_enrolled` `ce` on((`c`.`class_id` = `ce`.`class_id`))) group by `c`.`class_id` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `class_enrolled_count`  AS SELECT `c`.`class_id` AS `class_id`, count(`ce`.`user_id`) AS `num_students` FROM (`class` `c` left join `class_enrolled` `ce` on(`c`.`class_id` = `ce`.`class_id`)) GROUP BY `c`.`class_id` ;
 
 --
 -- Indexes for dumped tables
@@ -385,13 +291,13 @@ ALTER TABLE `assignments`
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `class_enrolled`
 --
 ALTER TABLE `class_enrolled`
-  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `comments`
@@ -427,7 +333,7 @@ ALTER TABLE `student_files`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -449,8 +355,8 @@ ALTER TABLE `class`
 -- Constraints for table `class_enrolled`
 --
 ALTER TABLE `class_enrolled`
-  ADD CONSTRAINT `class_enrolled_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
-  ADD CONSTRAINT `class_enrolled_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `class_enrolled_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `class_enrolled_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `comments`
@@ -481,8 +387,8 @@ ALTER TABLE `lectures`
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `student_files`
