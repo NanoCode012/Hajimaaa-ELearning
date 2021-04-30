f53e72e45fb37bd5775f0f4c93ddef1d705994d4
NanoCode012 <kevinvong@rocketmail.com>
2021-04-30 17:07:29 +0700

    Add email to class header (#64)

--- includes/classhead.php
+++ includes/classhead.php
@@ -1,8 +1,7 @@
-
 <div class="dashboard_container">
     <?php
     $class_id = $_GET['class_id'];
-    $stmt = $db_r->prepare('SELECT class_name,class_code,class_instructor FROM class WHERE class_id=?');
+    $stmt = $db_r->prepare('SELECT c.class_name,c.class_code,c.class_instructor, u.email FROM class c, users u WHERE c.class_id=? AND u.user_id = c.instructor_id');
     $stmt->execute([$class_id]);
     $get = $stmt->fetch();
     ?>
@@ -11,7 +10,7 @@
             <h1><?php echo $get['class_code']; ?> <?php echo $get['class_name']; ?></h1>
             <h4 class="edu_title">Instructor: <?php echo $get['class_instructor'] ?>
             </h4>
-            <span class="dashboard_instructor">Contact: Email@email.com</span>
+            <span class="dashboard_instructor">Email: <?php echo $get['email'] ?></span>
         </div>
     </div>
 </div>
