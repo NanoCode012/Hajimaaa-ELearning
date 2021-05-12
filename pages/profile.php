b541fa174b54dfeaeed1ea3548b84d87e981969c
NanoCode012 <kevinvong@rocketmail.com>
2021-05-12 23:25:19 +0700

    Added class teaching for teacher

--- pages/profile.php
+++ pages/profile.php
@@ -18,12 +18,24 @@ $short_acc_type = $account_type = "";
 
 $fullname = $firstname . " " . $lastname;
 
-if($user_type == 0) {
-  $account_type = "Student";
-  $class_text = " Enrolled in";
+if ($user_type == 0) {
+    $account_type = "Student";
+    $class_text = " Enrolled in";
+
+    $sql2 = 'SELECT COUNT(user_id) FROM class_enrolled WHERE user_id=?';
+    $stmt2 = $db_r->prepare($sql2);
+    $stmt2->execute([$current_user_id]);
+    $classes_enrolled = $stmt2->fetch();
+    $num_class = $classes_enrolled["COUNT(user_id)"];
 } else if ($user_type == 1) {
-  $account_type = "Teacher";
-  $class_text = " Teaching";
+    $account_type = "Teacher";
+    $class_text = " Teaching";
+
+    $sql2 = 'SELECT COUNT(instructor_id) FROM class WHERE instructor_id=?';
+    $stmt2 = $db_r->prepare($sql2);
+    $stmt2->execute([$current_user_id]);
+    $classes_teaching = $stmt2->fetch();
+    $num_class = $classes_teaching["COUNT(instructor_id)"];
 }
 
 
@@ -36,11 +48,7 @@ $zip = $user["zip"];
 $about = $user["about"];
 $title = $user["title"];
 
-$sql2 = 'SELECT COUNT(user_id) FROM class_enrolled WHERE user_id=?';
-$stmt2 = $db_r->prepare($sql2);
-$stmt2->execute([$current_user_id]);
-$classes_enrolled = $stmt2->fetch();
-$num_class = $classes_enrolled["COUNT(user_id)"];
+
 ?>
 
 
@@ -89,7 +97,8 @@ $num_class = $classes_enrolled["COUNT(user_id)"];
                                                 <div class="viewer_header">
                                                     <h1> <?php echo $fullname; ?> </h1>
                                                     <ul>
-                                                        <li><strong><?php echo $num_class; ?></strong> Classes <?php echo $class_text; ?></li>
+                                                        <li><strong><?php echo $num_class; ?></strong> Classes
+                                                            <?php echo $class_text; ?></li>
                                                     </ul>
                                                 </div>
                                                 <!-- archive -->
@@ -101,7 +110,7 @@ $num_class = $classes_enrolled["COUNT(user_id)"];
                                                         <li class="elite unlock"><i class="ti-medall"></i></li>
                                                         <li class="power unlock"><i class="ti-crown"></i></li> -->
                                                     </ul>
-                                                <!-- /archive -->
+                                                    <!-- /archive -->
                                                 </div>
                                             </div>
                                         </div>
@@ -123,71 +132,83 @@ $num_class = $classes_enrolled["COUNT(user_id)"];
                                     <div class="dashboard_container_body p-4">
                                         <!-- Basic info -->
                                         <form action="?p=editpfp" method="post">
-                                        <div class="submit-section">
-                                            <div class="form-row">
+                                            <div class="submit-section">
+                                                <div class="form-row">
 
-                                                <div class="form-group col-md-6">
-                                                    <label>First Name</label>
-                                                    <input type="text" class="form-control" name="firstname" value="<?php echo $firstname; ?>">
+                                                    <div class="form-group col-md-6">
+                                                        <label>First Name</label>
+                                                        <input type="text" class="form-control" name="firstname"
+                                                            value="<?php echo $firstname; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Last Name</label>
+                                                        <input type="text" class="form-control" name="lastname"
+                                                            value="<?php echo $lastname; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Username</label>
+                                                        <input type="text" class="form-control" name="username"
+                                                            value="<?php echo $username; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Title</label>
+                                                        <input type="text" class="form-control" name="title"
+                                                            value="<?php echo $title; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Email</label>
+                                                        <input type="text" class="form-control" name="email"
+                                                            value="<?php echo $email; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Phone</label>
+                                                        <input type="text" class="form-control" name="phone"
+                                                            value="<?php echo $phone; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Address</label>
+                                                        <input type="text" class="form-control" name="address"
+                                                            value="<?php echo $address; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>City</label>
+                                                        <input type="text" class="form-control" name="city"
+                                                            value="<?php echo $city; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>State</label>
+                                                        <input type="text" class="form-control" name="state"
+                                                            value="<?php echo $state; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-6">
+                                                        <label>Zip</label>
+                                                        <input type="text" class="form-control" name="zip"
+                                                            value="<?php echo $zip; ?>">
+                                                    </div>
+
+                                                    <div class="form-group col-md-12">
+                                                        <label>About</label>
+                                                        <textarea class="form-control"
+                                                            name="about"><?php echo $about; ?></textarea>
+                                                    </div>
+
+                                                    <div class="form-group col-lg-12 col-md-12">
+                                                        <button class="btn btn-theme" type="submit">Save
+                                                            Changes</button>
+                                                    </div>
+
                                                 </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Last Name</label>
-                                                    <input type="text" class="form-control" name="lastname" value="<?php echo $lastname; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Username</label>
-                                                    <input type="text" class="form-control" name="username" value="<?php echo $username; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Title</label>
-                                                    <input type="text" class="form-control" name="title" value="<?php echo $title; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Email</label>
-                                                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Phone</label>
-                                                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Address</label>
-                                                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>City</label>
-                                                    <input type="text" class="form-control" name="city" value="<?php echo $city; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>State</label>
-                                                    <input type="text" class="form-control" name="state" value="<?php echo $state; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-6">
-                                                    <label>Zip</label>
-                                                    <input type="text" class="form-control" name="zip" value="<?php echo $zip; ?>">
-                                                </div>
-
-                                                <div class="form-group col-md-12">
-                                                    <label>About</label>
-                                                    <textarea class="form-control" name="about"><?php echo $about; ?></textarea>
-                                                </div>
-
-                                                <div class="form-group col-lg-12 col-md-12">
-                                                    <button class="btn btn-theme" type="submit">Save Changes</button>
-                                                </div>
-
                                             </div>
-                                        </div>
-                                      </form>
+                                        </form>
                                         <!-- Basic info -->
                                     </div>
                                 </div>
